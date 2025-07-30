995de1acb08438723a4bff4d714b796322f484e7
Desta <destaanugrapratama@gmail.com>
2025-07-31 00:21:36 +0700

    fixed cors

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 // server/server.js
+const express = require('express');
+const app = express();
+
 const dotenv = require('dotenv');
+app.get('/', (req, res) => {
+  res.send('Haiiii love youu❤️');
+});
+
 dotenv.config();
 const generateCertificate = require('./utils/generateCertificate');
 const sendCertificate = require('./mailer/sendCertificate');
 
 async function handler(req, res) {
+  // CORS headers for serverless (Vercel)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method not allowed' });
     return;
